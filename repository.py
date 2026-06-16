af0c5a5976b32d131268e8ec47b91083803e37df
Karan Chawla <singhkaranpreet@seasiaconnect.com>
2026-06-16 18:02:35 +0530

    Updated

--- repository.py
+++ repository.py
@@ -1,5 +1,19 @@
+from datetime import datetime
+
+
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.is_email_verified = False
+
+
+class UserRepository:
+    def __init__(self):
+        self.users = {}
+
+    def get_by_username(self, username):
+        return self.users.get(username)
+
+    def save(self, user):
+        self.users[user.username] = user
