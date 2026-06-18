517129d1e66c172e7da376434508805e4c245a15
Karan Chawla <singhkaranpreet@seasiaconnect.com>
2026-06-18 18:47:02 +0530

    hk

--- api.py
+++ api.py
@@ -1,20 +1,9 @@
-from repository import UserRepository, User
+from repository import UserRepository
 from auth_service import AuthService
 
 
 repo = UserRepository()
-
-repo.save(
-    User(
-        username="john",
-        password="secret"
-    )
-)
-
 service = AuthService(repo)
 
-result = service.login("john", "secret")
-print(result)
-
-new_result = service.authenticate_user("john", "secret")
-print(new_result)
+profile = service.get_profile("john")
+print(profile.username)
